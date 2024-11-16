e3f1eebc5a21626f2ac071404390944e4d8faa38
Victor Nnaji <nnajivictor0@gmail.com>
2024-11-16 14:30:27 +0100

    Make predictions poll run in the client

--- utils/api-helpers/server.ts
+++ utils/api-helpers/server.ts
@@ -5,33 +5,15 @@ import type { cloudinaryFolders } from "@/types";
 import { getPredictionById } from "@/data/services/predictions.service";
 import { uploadImage } from "@/data/services/upload.service";
 
-const POLLING_INTERVAL = 2000;
-
 export const uploadImageToCloudinary = async (
   imageUrl: string,
   folder: cloudinaryFolders = "unblur-photos"
 ) => {
   const result = await uploadImage(imageUrl, folder);
   return result;
-  // return {
-  //   url: "https://res.cloudinary.com/victornnaji/image/upload/v1727945534/unblur-photos/sturdmc9j3nybd5r0zza.jpg",
-  // };
 };
 
-export const pollPredictionStatus = async (predictionId: string) => {
-  const checkStatus = async (): Promise<PredictionDto> => {
-    const data = await getPredictionById(predictionId);
 
-    if (!["starting", "processing"].includes(data.status || "")) {
-      return data;
-    }
-
-    await new Promise((resolve) => setTimeout(resolve, POLLING_INTERVAL));
-    return checkStatus();
-  };
-
-  return checkStatus();
-};
 
 export const getPredictionStartTime = async (predictionId: string) => {
   const data = await getPredictionById(predictionId);
